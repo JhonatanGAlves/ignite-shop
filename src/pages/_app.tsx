894e18e0d9cc8c8b715e6d6cf82c9fb8a7377524
JhonatanGAlves <jhonatangalves96@gmail.com>
2023-05-14 14:24:41 -0300

    feat: 🧑🏻‍💻 Creating global header in _app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
+import { AppContainer, Header } from "../styles/pages/app";
+import logoImg from "../assets/logo.svg";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppContainer>
+      <Header>
+        <img src={logoImg.src} alt="Logo header" />
+      </Header>
+
+      <Component {...pageProps} />
+    </AppContainer>
+  );
 }
