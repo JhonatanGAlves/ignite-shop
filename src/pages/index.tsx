bdb38fab8a5ea62e0c3a363d2664f7c60d195085
JhonatanGAlves <jhonatangalves96@gmail.com>
2023-05-14 15:37:15 -0300

    feat: 🧑🏻‍💻 Structuring the Home component with static data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import tShirt1 from "../assets/t-shirts/1.png";
+import tShirt2 from "../assets/t-shirts/2.png";
+import tShirt3 from "../assets/t-shirts/3.png";
+import tShirt4 from "../assets/t-shirts/4.png";
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={tShirt1} width={520} height={480} alt="" />
+        <footer>
+          <strong>T-Shirt X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={tShirt2} width={520} height={480} alt="" />
+        <footer>
+          <strong>T-Shirt Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
