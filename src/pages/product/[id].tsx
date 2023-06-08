c355b59534ef2da0242695b568206bca9c145f5c
JhonatanGAlves <jhonatangalves96@gmail.com>
2023-06-08 18:13:04 -0300

    feat: 🧑🏻‍💻 Calling products info from Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,30 +1,82 @@
+import { stripe } from "@/src/lib/stripe";
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from "@/src/styles/pages/product";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import Stripe from "stripe";
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+}
+
+export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>T-Shirt X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio et
-          maxime molestias reprehenderit atque aliquid minima distinctio
-          blanditiis necessitatibus libero, at temporibus! Repellat inventore
-          vitae beatae consequatur, dolores dignissimos id?
-        </p>
+        <p>{product.description}</p>
 
         <button>Buy now</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [{ params: { id: "prod_Ntm3cEMJ37lBVi" } }],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price:
+          price.unit_amount &&
+          new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          }).format(price.unit_amount / 100), // Handle units in cents. unit_amount came in cents and is * 100 to save to db.
+        description: product.description,
+      },
+      revalidate: 60 * 60 * 1, // 1 hour
+    },
+  };
+};
